1913645cfdacda6d8d865a16dec0bf738b063b9b
Izumi Zama <izumiz@izumiz.me>
2020-06-27 23:23:01 +0900

    revise git repository

---
+++ README.md
@@ -0,0 +1,9 @@
+# README.md
+
+[Go 言語で作るインタプリタ](https://www.oreilly.co.jp/books/9784873118222/)を写経するリポジトリ
+
+## Reference
+
+- [goenv](https://github.com/syndbg/goenv)
+- [direnv](https://github.com/direnv/direnv)
+- [Go 言語：文法基礎まとめ](https://qiita.com/HiromuMasuda0228/items/65b9a593275f769f6b69)
---
+++ monkey/lexer/lexer.go
@@ -0,0 +1,93 @@
+package lexer
+
+import "monkey/token"
+
+type Lexer struct {
+	input           string
+	position        int // 入力における現在の位置
+	readPosition 	int // 現在の次の文字
+	ch              byte // 現在検査中の文字
+}
+
+func New(input string) *Lexer {
+    l := &Lexer{input: input}
+    l.readChar()
+    return l
+}
+
+func (l *Lexer) readChar() {
+    if l.readPosition >= len(l.input) {
+        // この中に入ったら終端
+        // 0はASCIIコードの"NUL"文字に対応していて，chに代入することで，まだ読み込んでいない もしくは ファイルの終わり を表す
+        l.ch = 0;
+    } else {
+        // 最後の文字に到達していない場合の条件
+        // l.chに次の文字を代入する（セットしている）
+        l.ch = l.input[l.readPosition]
+    }
+    // l.postionは常に最後に読んだ場所を指す
+    l.position = l.readPosition
+    l.readPosition += 1
+}
+
+func (l *Lexer) NextToken() token.Token {
+    var tok token.Token
+
+    l.skipWhiteSpace()
+
+    switch l.ch {
+    case '=':
+        tok = newToken(token.ASSIGN, l.ch)
+    case ';':
+        tok = newToken(token.SEMICOLON, l.ch)
+    case '(':
+        tok = newToken(token.LPAREN, l.ch)
+    case ')':
+        tok = newToken(token.RPAREN, l.ch)
+    case ',':
+        tok = newToken(token.COMMA, l.ch)
+    case '+':
+        tok = newToken(token.PLUS, l.ch)
+    case '{':
+        tok = newToken(token.LBRACE, l.ch)
+    case '}':
+        tok = newToken(token.RBRACE, l.ch)
+    case 0:
+        tok.Literal = ""
+        tok.Type = token.EOF
+    default:
+        if isLetter(l.ch) {
+            tok.Literal = l.readIdentifier()
+            tok.Type = token.LookupIdent(tok.Literal)
+            return tok
+        } else {
+            tok = newToken(token.ILLEGAL, l.ch)
+        }
+    }
+
+    l.readChar()
+    return tok
+}
+
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func (l *Lexer) readIdentifier() string {
+    position := l.position
+    for isLetter(l.ch) {
+        l.readChar()
+    }
+    return l.input[position:l.position]
+}
+
+func isLetter(ch byte) bool {
+    return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func (l *Lexer) skipWhiteSpace() {
+    for l.ch == ' ' || l.ch == '\n' || l.ch == '\r' {
+        l.readChar()
+    }
+}
